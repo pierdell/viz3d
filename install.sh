cfb95682894bf4e857ab86b84ecbba9ea5a96766
pdell-kitware <59437242+pdell-kitware@users.noreply.github.com>
2022-03-09 19:25:44 +0100

    [fix] return value for failed install script

--- install.sh
+++ install.sh
@@ -13,26 +13,29 @@ install_dir=${src_dir}/install
 mkdir "${build_dir}"
 mkdir "${build_dir}/external"
 
-function check_command() { 
-	echo "$(pwd)"
- }
-
+check_status_code() {
+   if [ $1 -ne 0 ]
+   then
+	echo "[VIZ3D] Failure. Exiting."
+	exit 1
+   fi
+}
 cd "${build_dir}/external"
-check_command
+check_status_code $?
 
 echo "[CT_ICP] -- Generating External Dependencies";
 cmake -G "${CMakeGenerator}" -S ${src_dir}/external -DINSTALL_DIR=${install_dir}
-check_command
+check_status_code $?
 
 echo "[CT_ICP] -- Building External Dependencies"
 cmake --build . --config Release
-check_command
+check_status_code $?
 
 echo "[CT_ICP] -- Generating CMake File"
 cd ${build_dir}
 cmake -G "${CMakeGenerator}" -S ${src_dir} -DINSTALL_DIR=${install_dir}
-check_command
+check_status_code $?
 
 echo "[CT_ICP] -- Building Main Project"
 cmake --build . --config Release --target install
-check_command
+check_status_code $?
