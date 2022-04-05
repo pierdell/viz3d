670f74014f33cce19cc25533cd3fe88e83285551
pdell-kitware <pierre.dellenbach@kitware.com>
2022-04-05 16:51:14 +0200

    [wip] Add a Global Config to viz3d to persist parameters

---
+++ include/viz3d/config.h
@@ -0,0 +1,12 @@
+/* Copyright (C) Pierre Dellenbach - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Pierre Dellenbach <pierdell@hotmail.com>
+ */
+
+
+
+#ifndef VIZ3D_CONFIG_H
+#define VIZ3D_CONFIG_H
+
+#endif //VIZ3D_CONFIG_H
---
+++ test/test_config.cxx
@@ -0,0 +1,8 @@
+/* Copyright (C) Pierre Dellenbach - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Pierre Dellenbach <pierdell@hotmail.com>
+ */
+
+
+
